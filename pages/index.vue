b253fcf4f55c5a3ae3dd0f9240d6f951ea8a8225
Christian Paranas <christiannparanas@gmail.com>
2021-01-02 19:50:06 +0800

    fb post ui

--- pages/index.vue
+++ pages/index.vue
@@ -12,10 +12,25 @@
     <div class="posts-wrapper">
       <div class="title">Random Posts</div>
       <div class="post" v-for="post in postsArr" :key="post.postId">
-        <div class="postDate">{{ post. date }}</div>
-        <img :src="dynaImg(post.userPic)" alt="">
-        <div class="author">{{ post.author }}</div>
+        <div class="post_details">
+          <img :src="dynaImg(post.userPic)" alt="">
+          <div class="">
+            <div class="author">{{ post.author }}</div>
+            <div class="postDate">{{ post.date }}</div>
+          </div>
+          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+        </div>
         <div class="content">{{ post.content }}</div>
+        <div class="post_interact">
+          <div class="post_inter">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"></path></svg>
+            <div class="post_in">Like</div>
+          </div>
+          <div class="post_inter">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #fff"><path d="M12,2C6.486,2,2,5.589,2,10c0,2.908,1.898,5.516,5,6.934V22l5.34-4.005C17.697,17.852,22,14.32,22,10 C22,5.589,17.514,2,12,2z M12,16h-0.333L9,18v-2.417l-0.641-0.247C5.67,14.301,4,12.256,4,10c0-3.309,3.589-6,8-6s8,2.691,8,6 C20,13.309,16.411,16,12,16z"></path></svg>
+            <div class="post_in">Comment</div>
+          </div>
+        </div>
       </div>
     </div>
     
@@ -105,39 +120,68 @@ export default {
       }
 
       .post {
-        margin-top: 10px;
-        padding: 10px 0;
-        width: fit-content;
-        display: grid;
-        grid-template-columns: 50px 1fr;
-        position: relative;
-        z-index: -1;
+        margin: 10px 0 0;
+        padding: 5px 5px 10px;
+        background-color: #2d3748;
+        border-radius: 10px;
 
-        img {
-          border-radius: 50%;
-          width: 40px;
-          height: 40px;
-        }
+        .post_details {
+          display: grid;
+          grid-template-columns: 60px 1fr 30px;
 
-        .postDate {
-          position: absolute;
-          font-size: 8px;
-          top: 40px;
-          left: 50px;
-        }
+          img {
+            margin: 10px;
+            border-radius: 50%;
+            width: 40px;
+            height: 40px;
+          }
 
-        .author {
-          padding-top: 10px;
+          .postDate {
+            font-size: 8px;
+            top: 40px;
+            left: 65px;
+          }
+
+          .author {
+            padding-top: 10px;
+          }
         }
 
         .content {
-          grid-column-start: 2;
-          margin-top: 12px;
-          padding: 5px 15px;
-          width: fit-content;
-          border-radius: 10px;
           font-size: 12px;
-          border: 1px solid #2d3748;
+          padding: 0 10px;
+          margin: 10px 0;
+          word-wrap: break-word;
+        }
+
+        .post_interact {
+          margin-top: 20px;
+          padding: 0 20px;
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+
+          .post_inter {
+            font-size: 13px;
+            display: flex;
+            width: fit-content;
+            margin: 0 auto;
+            padding: 5px 10px;
+            border-radius: 5px;
+            cursor: pointer;
+
+            .post_in {
+              padding-top: 2px;
+            }
+
+            svg {
+              margin-right: 5px;
+            }
+          }
+
+          .post_inter:hover {
+            background-color: #1a202c;
+          }
+
         }
       }
     }
@@ -152,14 +196,14 @@ export default {
           padding: 10px 15px;
           outline: none;
           border: 0;
-          border-radius: 8px;
+          border-radius: 10px;
           background-color: #2d3748;
         }
 
         button {
           background-color: teal;
           padding: 10px 20px;
-          border-radius: 8px;
+          border-radius: 10px;
         }
       }
     }
