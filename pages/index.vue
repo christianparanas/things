735c3ed762652fd6ca6b10b67b8f961dc2a430fb
Christian Paranas <christiannparanas@gmail.com>
2021-01-03 17:02:34 +0800

    fix textarea newline text

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@
 
     <div class="create">
       <form action="" @submit.prevent="newPost">
-        <img :src="dynaImg(user.photoURL)" alt="">
-        <textarea v-model="userInput" placeholder="Write something" name="" cols="100%" rows="2"></textarea>
+        <img v-cloak :src="dynaImg(user.photoURL)" alt="">
+        <textarea v-model="userInput" placeholder="Write something" name="" cols="100%" rows="3"></textarea>
         <button name="submit">
           <svg class="w-6 h-6" style="fill: #fff" enable-background="new 0 0 24 24" height="512" viewBox="0 0 24 24" width="512" xmlns="http://www.w3.org/2000/svg"><path d="m8.75 17.612v4.638c0 .324.208.611.516.713.077.025.156.037.234.037.234 0 .46-.11.604-.306l2.713-3.692z"/><path d="m23.685.139c-.23-.163-.532-.185-.782-.054l-22.5 11.75c-.266.139-.423.423-.401.722.023.3.222.556.505.653l6.255 2.138 13.321-11.39-10.308 12.419 10.483 3.583c.078.026.16.04.242.04.136 0 .271-.037.39-.109.19-.116.319-.311.352-.53l2.75-18.5c.041-.28-.077-.558-.307-.722z"/></svg>
         </button>
@@ -24,7 +24,7 @@
           </div>
           <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
         </div>
-        <div class="content">{{ post.content }}</div>
+        <div class="content" v-html="post.content"></div>
         <div class="post_interact">
           <div class="post_inter">
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"></path></svg>
@@ -78,8 +78,14 @@ export default {
         this.fireDB.collection("posts")
         .get().then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
+            // console.log(doc.data().content.replace(/(?:\r\n|\r|\n)/g, '<br />'))
             this.postsArr.push(doc.data());
           });
+
+          // replace content newline with a <br /> to render it a newline
+          this.postsArr.forEach(obj =>  {
+            obj.content = obj.content.replace(/(?:\r\n|\r|\n)/g, '<br />')
+          })
         });
       },
 
@@ -95,7 +101,7 @@ export default {
         });
       },
     },
-    // before create check if user authenticated
+    // before create check if user is authenticated
      beforeCreate() {
       this.$fire.auth.onAuthStateChanged((user) => {
       if (user) {
@@ -120,7 +126,7 @@ export default {
     }
 
     .posts-wrapper {
-      padding: 20px;
+      padding: 30px 20px;
 
       hr {
         border-top: 1px solid #2d3748;
@@ -128,7 +134,7 @@ export default {
 
       .post {
         margin: 10px 0 0;
-        padding: 5px 5px 10px;
+        padding: 5px 5px 0;
         background-color: #2d3748;
         border-radius: 5px;
 
@@ -162,7 +168,7 @@ export default {
         }
 
         .post_interact {
-          margin: 20px 10px 0;
+          margin: 20px 5px 0;
           padding: 10px 20px;
           display: grid;
           border-top: 1px solid grey;
