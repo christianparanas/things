fe8b71e182f1d83629f081f795225769a989ad28
Christian Paranas <christiannparanas@gmail.com>
2021-01-04 15:32:54 +0800

    msg nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -19,12 +19,30 @@
 							</div>
 						</div>
 					</NuxtLink>
+
+					<NuxtLink to="/messages">
+					<div class="users_nav">
+						<svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" height="226px" width="224px" version="1.1" viewBox="0 0 224 226">
+						 <defs>
+						  <linearGradient id="a" y1="6.76%" x2="50%" x1="50%" y2="95.6%">
+						   <stop stop-color="#00C6FF" offset="0"/>
+						   <stop stop-color="#0068FF" offset="1"/>
+						  </linearGradient>
+						 </defs>
+						<path fill="url(#a)" d="m41.255 185.52v40.2l37.589-21.37c10.478 3.02 21.616 4.65 33.156 4.65 61.86 0 112-46.79 112-104.5 0-57.714-50.14-104.5-112-104.5-61.856 0-112 46.786-112 104.5 0 32.68 16.078 61.86 41.255 81.02z"/>
+						<path fill="#fff" d="m100.04 75.878l-60.401 63.952 54.97-30.16 28.721 30.16 60.06-63.952-54.36 29.632-28.99-29.632z"/>
+						</svg>
+
+						<div class="userTit">Messages</div>
+					</div>
+					</NuxtLink>
 					<NuxtLink to="/users">
 					<div class="users_nav">
 						<svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #fff"><circle fill="none" cx="12" cy="7" r="3"></circle><path d="M12 2C9.243 2 7 4.243 7 7s2.243 5 5 5 5-2.243 5-5S14.757 2 12 2zM12 10c-1.654 0-3-1.346-3-3s1.346-3 3-3 3 1.346 3 3S13.654 10 12 10zM21 21v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h2v-1c0-2.757 2.243-5 5-5h4c2.757 0 5 2.243 5 5v1H21z"></path></svg>
 						<div class="userTit">Users</div>
 					</div>
 					</NuxtLink>
+					
 					<div class="logout" @click="signOut">
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12,21c4.411,0,8-3.589,8-8c0-3.35-2.072-6.221-5-7.411v2.223c1.79,1.04,3,2.973,3,5.188c0,3.309-2.691,6-6,6s-6-2.691-6-6 c0-2.215,1.21-4.149,3-5.188V5.589C6.072,6.779,4,9.65,4,13C4,17.411,7.589,21,12,21z"></path><path d="M11 2H13V12H11z"></path></svg>
 						<div class="logtext">Logout</div>
@@ -105,7 +123,6 @@
 
 				.users_nav {
 					display: flex;
-					margin: 30px 0;
 					padding: 10px 0;
 
 					.userTit {
@@ -115,6 +132,7 @@
 
 				.nav_profile {
 					display: flex;
+					margin-bottom: 20px;
 
 					img {
 						border-radius: 50%;
@@ -129,7 +147,8 @@
 						}
 
 						.see {
-							font-size: 10px;
+							font-size: 8px;
+							background-color: #1a202c;
 						}
 					}
 				}
---
+++ pages/messages.vue
@@ -0,0 +1,81 @@
+<template>
+	<div class="convo-main-container">
+		<div class="title">
+			<svg class="w-10 h-10" xmlns="http://www.w3.org/2000/svg" height="226px" width="224px" version="1.1" viewBox="0 0 224 226">
+				<defs>
+					<linearGradient id="a" y1="6.76%" x2="50%" x1="50%" y2="95.6%">
+						<stop stop-color="#00C6FF" offset="0"/>
+						<stop stop-color="#0068FF" offset="1"/>
+				  </linearGradient>
+				</defs>
+			<path fill="url(#a)" d="m41.255 185.52v40.2l37.589-21.37c10.478 3.02 21.616 4.65 33.156 4.65 61.86 0 112-46.79 112-104.5 0-57.714-50.14-104.5-112-104.5-61.856 0-112 46.786-112 104.5 0 32.68 16.078 61.86 41.255 81.02z"/>
+				<path fill="#fff" d="m100.04 75.878l-60.401 63.952 54.97-30.16 28.721 30.16 60.06-63.952-54.36 29.632-28.99-29.632z"/>
+			</svg>
+			<div class="tit">Messenger</div>
+		</div>
+		<NuxtLink to="/">
+			<div class="back">
+				<svg class="w-7 h-7" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #fff"><path d="M21 11L6.414 11 11.707 5.707 10.293 4.293 2.586 12 10.293 19.707 11.707 18.293 6.414 13 21 13z"></path></svg>
+			</div>
+		</NuxtLink>
+
+		<div class="convo-container">
+			<div class="convo">
+				<img src="https://avatars.dicebear.com/4.5/api/initials/Member's%20Chatbox.svg" alt="">
+				<div class="name">Member's Chatbox</div>
+			</div>
+		</div>
+
+	</div>
+</template>
+
+
+
+<style lang="scss" scoped>
+	.convo-main-container {
+		padding: 20px;
+
+		.title {
+			display: flex;
+			justify-content: center;
+
+			svg {
+				margin-right: 5px;
+			}
+
+			.tit {
+				margin-top: 8px;
+			}
+		}
+
+		.back {
+			position: absolute;
+			top: 20px;
+			left: 15px;
+			background-color: #2d3748;
+			width: fit-content;
+			padding: 7px;
+			border-radius: 50%;
+		}
+
+		.convo-container {
+			margin-top: 60px;
+
+			.convo {
+				padding: 10px;
+				background-color: #2d3748;
+				border-radius: 10px;
+				display: flex;
+
+				img {
+					width: 50px;
+					border-radius: 50%;
+				}
+
+				.name {
+					margin: 15px 0 0 10px;
+				}
+			}
+		}
+	}
+</style>
